f655433ca42e5fbaecbdee0b63fa36a3d6fc4c15
Chriss-Cross <60609935+Chriss-Cross@users.noreply.github.com>
2021-10-04 15:58:48 +0530

    Add files via upload

--- StellarStage-master/screens/SpaceCraft.js
+++ StellarStage-master/screens/SpaceCraft.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-export default class SpaceCraft extends Component {
+export default class SpaceCraftScreen extends Component {
     render() {
         return (
             <View
